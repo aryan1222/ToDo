96ee79e0257f6ee48db7d49072113f33fa99a33f
aryan1222 <chhabraaryan12@gmail.com>
2020-06-30 09:56:20 +0530

    todo

---
+++ script.js
@@ -0,0 +1,50 @@
+const addTask = document.querySelector('#taskForm');
+const taskList = document.querySelector('#taskList');
+const clrbtn = document.querySelector('#clear')
+const taskInput = document.querySelector('#taskInput');
+
+const task = document.querySelector('#filter');
+
+// Add task
+addTask.addEventListener('submit',addNewTask);
+
+// Remove Task
+taskList.addEventListener('click',removeTask);
+
+// Clear All
+clrbtn.addEventListener('click',clearAll);
+
+function addNewTask(e){
+    if(taskInput.value ===''){
+        alert('Add Task')
+    }else{
+        const div = document.createElement('div');
+        div.className = 'group';
+
+        const li = document.createElement('li');
+        li.className = 'task-content';
+        li.appendChild(document.createTextNode(taskInput.value))
+        div.appendChild(li);
+
+        const link = document.createElement('a');
+        link.className = 'delete-task';
+        link.innerHTML = '<button class="delete group">-</button>';
+        div.appendChild(link);
+
+        taskList.appendChild(div);
+
+        taskInput.value = '';        
+    }
+
+    e.preventDefault();
+}
+
+function removeTask(e){
+    if(e.target.parentElement.classList.contains('delete-task')){
+        e.target.parentElement.parentElement.remove();
+    }
+}
+
+function clearAll(){
+    taskList.innerHTML ='';
+}
